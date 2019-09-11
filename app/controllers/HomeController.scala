49de9f2ee78bed14e4ad101a0e0d3323553bc4d5
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-09-11 18:50:03 +0100

    Updated routes and method name for downloading file

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,29 +3,17 @@ package controllers
 import forms.TeamForm
 import javax.inject._
 import models.Team
-import play.api._
 import play.api.mvc._
 import services.FixtureService
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, fixtureService: FixtureService) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index(TeamForm.form))
   }
 
-  def teamSelection(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def downloadCalendar(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     TeamForm.form.bindFromRequest.fold(
       formWithErrors => {
         BadRequest(formWithErrors.errors.toString)
