633dca21d615972e6547c79af7f25d0426e5c9b5
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-09-13 23:18:01 +0100

    fixed upload controller locally

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -1,18 +1,10 @@
 package controllers
 
 import java.io.File
-import java.nio.file.attribute.PosixFilePermission._
-import java.nio.file.attribute.PosixFilePermissions
-import java.nio.file.{Paths, Files => JFiles}
 
-import akka.stream.IOResult
-import akka.stream.scaladsl._
-import akka.util.ByteString
 import javax.inject._
-import play.api.libs.streams._
-import play.api.mvc.MultipartFormData.FilePart
+import play.api.libs.Files
 import play.api.mvc._
-import play.core.parsers.Multipart.FileInfo
 import services.FixtureService
 
 import scala.concurrent.ExecutionContext
@@ -25,21 +17,14 @@ class UploadController @Inject()(cc: ControllerComponents, fixtureService: Fixtu
   }
 
 
-  def uploadFile = Action(parse.temporaryFile) { request =>
-    request.body.moveTo(Paths.get("./app/resources/Pool fixtures.csv"), replace = true)
+  def uploadFile: Action[MultipartFormData[Files.TemporaryFile]] = Action(parse.multipartFormData) { request =>
+    request.body.file("fileUpload").map { file =>
+      val videoFilename = file.filename
+      val contentType = file.contentType.get
+      file.ref.moveFileTo(new File("./app/resources/" + file.filename), replace = true)
+    }.getOrElse {
+      Redirect(routes.UploadController.uploadPage)
+    }
     Ok("File has been uploaded")
   }
-
-
-
-//  def uploadFile: Action[MultipartFormData[Files.TemporaryFile]] = Action(parse.multipartFormData) { request =>
-//    request.body.file("fileUpload").map { video =>
-//      val videoFilename = video.filename
-//      val contentType = video.contentType.get
-//      video.ref.moveFileTo(new File("./app/resources/" + video.filename), replace = true)
-//    }.getOrElse {
-//      Redirect(routes.UploadController.uploadPage)
-//    }
-//    Ok("File has been uploaded")
-//  }
 }
