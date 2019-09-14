a83b0a08615d2c6e0060b16c5847327bb649f6f3
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-09-14 09:28:53 +0100

    add rootpath for new file creation

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -3,6 +3,7 @@ package controllers
 import java.io.File
 
 import javax.inject._
+import play.api.Environment
 import play.api.libs.Files
 import play.api.mvc._
 import services.FixtureService
@@ -10,18 +11,17 @@ import services.FixtureService
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class UploadController @Inject()(cc: ControllerComponents, fixtureService: FixtureService)(implicit ec: ExecutionContext) extends AbstractController(cc) with play.api.i18n.I18nSupport {
+class UploadController @Inject()(cc: ControllerComponents, fixtureService: FixtureService, environment: Environment)(implicit ec: ExecutionContext) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
   def uploadPage: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.upload("File Upload In Play"))
   }
-
-
+  
   def uploadFile: Action[MultipartFormData[Files.TemporaryFile]] = Action(parse.multipartFormData) { request =>
     request.body.file("fileUpload").map { file =>
       val filename = file.filename
       if (filename.takeRight(4) == ".csv") {
-        file.ref.moveFileTo(new File("./app/resources/" + file.filename), replace = true)
+        file.ref.moveFileTo(new File(environment.rootPath + "/app/resources/" + file.filename), replace = true)
         Ok("File has been uploaded")
       } else {
         Ok("File type is incorrect")
